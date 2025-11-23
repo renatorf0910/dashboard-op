c89d9081ea9d0ec78b8a217bc7ffa200a87bcc3a
renatorf0910 <renatorf0910@gmail.com>
2025-11-23 16:53:43 -0300

    remove animation about jump into scroll

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -56,7 +56,7 @@ function DrawerContent({
       <DrawerPrimitive.Content
         data-slot="drawer-content"
         className={cn(
-          "group/drawer-content bg-background fixed z-50 flex flex-col",
+          "group/drawer-content bg-background fixed z-50 flex flex-col will-change-[transform]",
           "data-[state=open]:animate-in data-[state=closed]:animate-out",
           "data-[state=open]:slide-in-from-right data-[state=closed]:slide-out-to-right",
           "h-[100vh] data-[vaul-drawer-direction=right]:w-[40%] md:data-[vaul-drawer-direction=right]:w-[30%]",
